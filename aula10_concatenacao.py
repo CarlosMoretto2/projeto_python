f8b921926bac4fb84de4ea80939151fd5b4b4ddc
CarlosMoretto2 <carlos.morettobr2@gmail.com>
2023-04-28 14:26:30 -0300

    Continuação das variáveis

---
+++ aula10_concatenacao.py
@@ -0,0 +1,10 @@
+concatenacao = 'a' + 'b' + 'c'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
+
+concatenacao2 = 'nome' + ' ' + 'sobrenome'
+print(concatenacao2)
